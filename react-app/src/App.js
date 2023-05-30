6881e4b6e7a91e4aceb352499d274257776703dd
CHHyuk <gksgur1038@naver.com>
2023-05-30 22:26:51 +0900

    05/30 리액트 기초다지기

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,34 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-function Header() {
+function Header(props) {
   return <header>
-  <h1><a href="">React</a></h1>
+  <h1><a href="">{props.title}</a></h1>
 </header>
 }
 
-function Nav() {
+function Nav(props) {
+  const lis = []
+
+  for(let i = 0 ; i< props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
+
   return (<nav>
     <ul>
-      <li><a href="">html</a></li>
-      <li><a href="">css</a></li>
-      <li><a href="">js</a></li>
+      {lis}
     </ul>
   </nav>)
 }
 
-function Article() {
+function Article(props) {
   return (<article>
-    <h2>welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>)
 }
 
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'js', body:'js is ...'}
+  ]
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="REACT"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB"></Article>
+      <Article title="Hi" body="Hello, REACT"></Article>
     </div>
   );
 }
